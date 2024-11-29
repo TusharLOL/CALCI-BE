b59dfc90644a63e9fe4861e20959bfa3edc1d012
Tushar Dontulwar <tushdontulwar@gmail.com>
2024-11-29 21:46:04 +0530

    first commit

--- apps/calculator/route.py
+++ apps/calculator/route.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 import base64
 from io import BytesIO
 from apps.calculator.utils import analyze_image
@@ -12,9 +12,13 @@ async def run(data: ImageData):
     image_data = base64.b64decode(data.image.split(",")[1])  # Assumes data:image/png;base64,<data>
     image_bytes = BytesIO(image_data)
     image = Image.open(image_bytes)
-    responses = analyze_image(image, dict_of_vars=data.dict_of_vars)
+    try:
+        responses = analyze_image(image, dict_of_vars=data.dict_of_vars)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error analyzing image: {e}")
+    
     data = []
     for response in responses:
         data.append(response)
-    print('response in route: ', response)
+        print('response in route: ', response)
     return {"message": "Image processed", "data": data, "status": "success"}
--- constants.py
+++ constants.py
@@ -2,8 +2,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SERVER_URL = 'localhost'
-PORT = '8900'
+SERVER_URL = 'localhost';
+PORT = 8900
 ENV = 'dev'
-
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
